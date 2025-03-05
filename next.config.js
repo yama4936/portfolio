79dd9ae5750974760f4b7734c0d9d814a059dc2e
yama4936 <t323088@chukyo-u.ac.jp>
2025-03-06 00:49:25 +0900

    デプロイ用

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export", // 静的サイトとしてエクスポート
+  basePath: "/portfolio", // ← GitHubのリポジトリ名に変更
+  assetPrefix: "/portfolio/", // ← これも変更
 };
 
 module.exports = nextConfig;
